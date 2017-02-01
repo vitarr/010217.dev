80eb0b44e224da794c67de0d951d86100815fd05
sasha44 <12345@i.ua>
2017-02-01 16:41:15 +0200

    обработка авторизации

--- 01022017.dev/autorisation.php
+++ 01022017.dev/autorisation.php
@@ -1,17 +1,37 @@
+<?php
+$users = array(
+    admin => array(
+        login => 'admin',
+        password => 'qwerty'
+    )
+);
+$login = filter_input(INPUT_POST, 'login');
+$pass = filter_input(INPUT_POST, 'pass');
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+            body{
+                padding-top: 20px;
+            }
+            h2{
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <?php if($login == $users['admin']['login'] && $pass == $users['admin']['password']): ?>
+        <h2>Вы авторизованы</h2>
+        <?php else: ?>
+        <h2>Неправиьный логин/пароль</h2>
+        <?php endif; ?>
     </body>
 </html>
--- 01022017.dev/index.php
+++ 01022017.dev/index.php
@@ -24,33 +24,19 @@
                     <div class="well well-sm">
                         <form class="form-horizontal" action="autorisation.php" method="post">
                             <fieldset>
-                                <legend class="text-center">Contact us</legend>
-
-                                <!-- Name input-->
+                                <legend class="text-center">Sign in:</legend>
                                 <div class="form-group">
-                                    <label class="col-md-3 control-label" for="name">Name</label>
+                                    <label class="col-md-3 control-label" for="name">Login:</label>
                                     <div class="col-md-9">
-                                        <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+                                        <input id="name" name="login" type="text" placeholder="Your login" class="form-control">
                                     </div>
                                 </div>
-
-                                <!-- Email input-->
                                 <div class="form-group">
-                                    <label class="col-md-3 control-label" for="email">Your E-mail</label>
+                                    <label class="col-md-3 control-label" for="pass">Password:</label>
                                     <div class="col-md-9">
-                                        <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+                                        <input id="pass" name="pass" type="password" placeholder="Your password" class="form-control">
                                     </div>
                                 </div>
-
-                                <!-- Message body -->
-                                <div class="form-group">
-                                    <label class="col-md-3 control-label" for="message">Your message</label>
-                                    <div class="col-md-9">
-                                        <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-                                    </div>
-                                </div>
-
-                                <!-- Form actions -->
                                 <div class="form-group">
                                     <div class="col-md-12 text-right">
                                         <button type="submit" class="btn btn-primary btn-lg">Submit</button>
